b5f2eace95e642b14e0d792bdbeab9d4d11290a0
Eyespot <eger_np_@inbox.ru>
2019-10-15 23:36:43 +0300

    removes empty lines, simplifies App declaration

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import MainScreen from './../main-screen/main-screen.jsx';
 
-const App = () => {
-
-  return <MainScreen/>;
-};
+const App = () => (<MainScreen/>);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDom from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
-
   ReactDom.render(
       <App/>,
       document.querySelector(`#root`)
